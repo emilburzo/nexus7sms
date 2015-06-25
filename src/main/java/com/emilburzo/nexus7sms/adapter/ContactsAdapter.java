75187cd08f381fd5dd44499d4007929365ec163a
Emil Burzo <eaglex@gmail.com>
2015-06-25 10:11:17 +0300

    add avatar to contacts

--- src/main/java/com/emilburzo/nexus7sms/adapter/ContactsAdapter.java
+++ src/main/java/com/emilburzo/nexus7sms/adapter/ContactsAdapter.java
@@ -1,22 +1,27 @@
 package com.emilburzo.nexus7sms.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.emilburzo.nexus7sms.R;
+import com.emilburzo.nexus7sms.misc.Utils;
 import com.emilburzo.nexus7sms.pojo.Contact;
 
 import java.util.List;
 
 public class ContactsAdapter extends BaseAdapter {
 
+    private final Context context;
     private final List<Contact> contactList;
     private static LayoutInflater inflater = null;
 
     public ContactsAdapter(Context context, List<Contact> contactList) {
+        this.context = context;
         this.contactList = contactList;
 
         inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -47,8 +52,23 @@ public class ContactsAdapter extends BaseAdapter {
 
         Contact contact = contactList.get(position);
 
+        // picture
+        ImageView picture = (ImageView) vi.findViewById(R.id.picture);
+
+        Bitmap photo = Utils.getContactPhoto(context, contact.phone);
+
+        if (photo == null) {
+            picture.setImageDrawable(Utils.getContactTextPhoto(context, contact.phone));
+        } else {
+            picture.setImageBitmap(photo);
+        }
+
+        // name
+
         TextView name = (TextView) vi.findViewById(R.id.contactName);
         name.setText(contact.name);
+
+        // phone
 
         TextView phone = (TextView) vi.findViewById(R.id.contactPhone);
         if (contact.phoneType == null) {
